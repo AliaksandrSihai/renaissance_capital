7bf7b8b194e9b11fb40ad1124275cb4a5cbad61d
AliaksandrSihai <sigai.aleksandr@mail.ru>
2024-02-15 13:42:25 +0200

    init commit

---
+++ main.py
@@ -0,0 +1,32 @@
+import os
+import sqlite3
+from lxml import etree
+import logging
+from db import DBConnect
+
+
+xml_file = 'answer.xml'
+logging.basicConfig(level=logging.INFO)
+
+
+if __name__ == "__main__":
+    db = DBConnect("test")
+    db.create_table("test")
+
+    if os.path.isfile(xml_file):
+        root_element = etree.parse(xml_file).getroot()
+
+        for date_elem in root_element.find("importStatus"):
+            for type_elem in date_elem.findall("type"):
+                for label_elem in type_elem.findall("label"):
+                    label = label_elem.get("value")
+                    status = label_elem.find("importedStatus").text
+                    db.update_status(label=label, status=status)
+                    logging.info(f"Статус {label} обновлен {status} ")
+
+        with sqlite3.connect("test") as conn:
+            cursor = conn.cursor()
+            result = cursor.execute("""SELECT * FROM test""")
+            logging.info(f"Записи из бд {result.fetchall()}")
+    else:
+        logging.error("XML-файл не найден.")
